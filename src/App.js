bac2ab50f4eb185b11d48fd2710e9babb0f9abbb
acarlseen <110647552+acarlseen@users.noreply.github.com>
2024-03-20 20:39:36 -0700

    added time travel, updated reset

--- src/App.js
+++ src/App.js
@@ -63,17 +63,40 @@ function Board({ xIsNext, squares, onPlay }) {
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0)
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length-1)
     setXIsNext(!xIsNext);
   }
+
+  function jumpTo(nextMove){
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 == 0)
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
 
   function resetGame(){
     const nextSquares = [Array(9).fill(null)];
     setXIsNext(true);
     setHistory(nextSquares);
+    setCurrentMove(0);
   }
 
   return (
@@ -83,7 +106,7 @@ export default function Game() {
         <ClearBoard onClear={() => resetGame()} />
       </div>
       <div className="game-info">
-        <ol>{/*TODO*/}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
